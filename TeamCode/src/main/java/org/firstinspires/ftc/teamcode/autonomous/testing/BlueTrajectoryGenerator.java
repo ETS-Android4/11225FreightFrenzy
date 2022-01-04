f5dc186d89622baf971f6c80034c9c03233893ed
Delta11225 <lcsdeltarobotics@gmail.com>
2022-01-04 10:48:45 -0500

    Began work on generating blue trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/testing/BlueTrajectoryGenerator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/testing/BlueTrajectoryGenerator.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.autonomous.testing;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 
+import org.firstinspires.ftc.teamcode.autonomous.PathType;
 import org.firstinspires.ftc.teamcode.autonomous.enums.ParkingMethod;
 import org.firstinspires.ftc.teamcode.autonomous.enums.Position;
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
@@ -21,7 +24,79 @@ public class BlueTrajectoryGenerator extends TrajectoryGenerator {
     }
 
     public ArrayList<ArrayList<Trajectory>> generateTrajectories() {
-        // TODO generate trajectories based on position and color
-        throw new NotImplementedError();
+        ArrayList<Double[]> trajectory1 = new ArrayList<>();
+        ArrayList<Double[]> trajectory2 = new ArrayList<>();
+        ArrayList<Double[]> trajectory3 = new ArrayList<>();
+
+        ArrayList<ArrayList<Trajectory>> finalTrajs = new ArrayList<>();
+
+        if (position == Position.BACK) {
+            Vector2d vector = new Vector2d(8.0, 61.5);
+            Pose2d startPose = new Pose2d(vector, Math.toRadians(270));
+
+            drive.setPoseEstimate(startPose);
+
+            // moving to the shipping hub
+            generateTrajectoryListItem(-7, 45, 255, PathType.LINE_TO_LINEAR, trajectory1);
+            if (parkingMethod == ParkingMethod.WALL) {
+                // getting in position to cross the field and park
+                generateTrajectoryListItem(0, 68.25, 180, PathType.LINE_TO_LINEAR, trajectory3);
+                // hugging the wall and moving into the warehouse
+                generateTrajectoryListItem(50, 71.25, 180, PathType.LINE_TO_LINEAR, trajectory3);
+            } else if (parkingMethod == ParkingMethod.BARRIER) {
+                // getting in position to cross the field and park
+                generateTrajectoryListItem(7, 50, 180, PathType.LINE_TO_LINEAR, trajectory3);
+                generateTrajectoryListItem(50, 40, 180, PathType.LINE_TO_LINEAR, trajectory3);
+            }
+
+            ArrayList<Trajectory> compTraj1 = compileTrajectoryList(startPose, trajectory1);
+            ArrayList<Trajectory> compTraj2 = new ArrayList<>();
+            ArrayList<Trajectory> compTraj3 = compileTrajectoryList(compTraj1.get(compTraj1.size() - 1).end(), trajectory3);
+
+
+            finalTrajs.add(compTraj1);
+            finalTrajs.add(compTraj2);
+            finalTrajs.add(compTraj3);
+
+
+        } else if (position == Position.FRONT) {
+            Vector2d vector = new Vector2d(-32.25, 61.5);
+            Pose2d startPose = new Pose2d(vector, Math.toRadians(270));
+
+            drive.setPoseEstimate(startPose);
+
+            // moving to the shipping hub
+            generateTrajectoryListItem(-10, 47, 310, 0, PathType.SPLINE_TO_LINEAR, trajectory1);
+            // getting in position to dump
+            generateTrajectoryListItem(-12, 36.5, PathType.LINE_TO_CONSTANT, trajectory1);
+
+            // back up to avoid duck/tse
+            generateTrajectoryListItem(-17, 50, PathType.LINE_TO_CONSTANT, trajectory2);
+
+            // moving to the duck wheel
+            generateTrajectoryListItem(-60, 57, 210, PathType.LINE_TO_LINEAR, trajectory2);
+            //nudging up to the duck wheel
+            generateTrajectoryListItem(-63, 60, 240, PathType.LINE_TO_LINEAR, trajectory2);
+
+            if (parkingMethod == ParkingMethod.WALL) {
+                // getting in position to cross the field and park
+                generateTrajectoryListItem(-50, 68.25, 180, PathType.LINE_TO_LINEAR, trajectory3);
+                // hugging the wall and moving into the warehouse
+                generateTrajectoryListItem(50, 71.25, 180, PathType.LINE_TO_LINEAR, trajectory3);
+            } else if (parkingMethod == ParkingMethod.BARRIER) {
+                // getting in position to cross the field and park
+                generateTrajectoryListItem(7, 50, 180, PathType.LINE_TO_LINEAR, trajectory3);
+                generateTrajectoryListItem(50, 40, 180, PathType.LINE_TO_LINEAR, trajectory3);
+            }
+
+            ArrayList<Trajectory> compTraj1 = compileTrajectoryList(startPose, trajectory1);
+            ArrayList<Trajectory> compTraj2 = compileTrajectoryList(compTraj1.get(compTraj1.size() - 1).end(), trajectory2);
+            ArrayList<Trajectory> compTraj3 = compileTrajectoryList(compTraj2.get(compTraj2.size() - 1).end(), trajectory3);
+
+            finalTrajs.add(compTraj1);
+            finalTrajs.add(compTraj2);
+            finalTrajs.add(compTraj3);
+        }
+        return finalTrajs;
     }
 }
